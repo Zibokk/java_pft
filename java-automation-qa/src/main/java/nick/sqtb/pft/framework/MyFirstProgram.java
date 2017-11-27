544195dcb620650f7bd346b4811c881983f62f4a
Zibokk <nick.cornovan@gmail.com>
2017-11-26 19:27:44 -0800

    added comment and formating

--- java-automation-qa/src/main/java/nick/sqtb/pft/framework/MyFirstProgram.java
+++ java-automation-qa/src/main/java/nick/sqtb/pft/framework/MyFirstProgram.java
@@ -2,6 +2,7 @@ package nick.sqtb.pft.framework;
 
 public class MyFirstProgram {
 
+    // example of method or function
     public static void main(String[] args) {
         System.out.println("Hello, World!");
     }
