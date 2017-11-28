4c76eb45d312847834054b0f4eacb4f522631c7a
Zibokk <nick.cornovan@gmail.com>
2017-11-27 20:20:06 -0800

    functions and variables

--- java-automation-qa/src/main/java/nick/sqtb/pft/framework/MyFirstProgram.java
+++ java-automation-qa/src/main/java/nick/sqtb/pft/framework/MyFirstProgram.java
@@ -4,7 +4,27 @@ public class MyFirstProgram {
 
     // example of method or function
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        hello("java");
+        hello("Nick");
+
+        double x = 5;
+        System.out.println("Result " + x + " = " + area(x));
+
+        double a = 4;
+        double b = 5;
+        System.out.println("Result " + a + " and " + b + " = " + area2(a, b));
+    }
+
+    public static void hello(String somebody) {
+        System.out.println("Hello, " + somebody + "!");
+    }
+
+    public static double area(double l) {
+       return l * l;
+    }
+
+    public static double area2(double a, double b) {
+        return a * b;
     }
 
 }
