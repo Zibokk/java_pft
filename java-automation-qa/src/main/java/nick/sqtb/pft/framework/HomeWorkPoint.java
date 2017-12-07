0c6d3fb9bd13e2d59c9815d5bd3b0ef9673323cc
Zibokk <nick.cornovan@gmail.com>
2017-12-06 19:41:31 -0800

    Homework Point 3

--- java-automation-qa/src/main/java/nick/sqtb/pft/framework/HomeWorkPoint.java
+++ java-automation-qa/src/main/java/nick/sqtb/pft/framework/HomeWorkPoint.java
@@ -1,13 +1,15 @@
 package nick.sqtb.pft.framework;
-//
+
+
 public class HomeWorkPoint {
 
 
 public static void main(String[] args) {
 
         Point p1 = new Point(5, 5);
-        Point p2 = new Point(5, 3);
-        System.out.println("Result " + Point.distance(p1, p2));
+        Point p2 = new Point(4, 2);
+
+    System.out.println("Result = " + p1.distance(p2));
 
     }
 
