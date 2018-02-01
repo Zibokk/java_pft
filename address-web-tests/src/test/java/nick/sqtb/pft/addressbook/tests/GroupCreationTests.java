3d3913ee71b4bfee7fdfb473bdf871ead68d4af4
Zibokk <nick.cornovan@gmail.com>
2018-02-01 15:38:48 -0800

    Лекция 4.3. Количество элементов в коллекции

--- address-web-tests/src/test/java/nick/sqtb/pft/addressbook/tests/GroupCreationTests.java
+++ address-web-tests/src/test/java/nick/sqtb/pft/addressbook/tests/GroupCreationTests.java
@@ -1,6 +1,7 @@
 package nick.sqtb.pft.addressbook.tests;
 
 import nick.sqtb.pft.addressbook.model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 
@@ -9,11 +10,14 @@ public class GroupCreationTests extends TestBase {
     @Test
     public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
         app.getGroupHelper().createGroup(new GroupData("test1", null, null));
 //        app.getGroupHelper().initGroupCreation();
 //        app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
 //        app.getGroupHelper().submitGroupCreation();
 //        app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before + 1);
     }
 
 
--- address-web-tests/src/test/java/nick/sqtb/pft/addressbook/tests/GroupDeletionTests.java
+++ address-web-tests/src/test/java/nick/sqtb/pft/addressbook/tests/GroupDeletionTests.java
@@ -1,6 +1,7 @@
 package nick.sqtb.pft.addressbook.tests;
 
 import nick.sqtb.pft.addressbook.model.GroupData;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class GroupDeletionTests extends TestBase {
@@ -8,6 +9,7 @@ public class GroupDeletionTests extends TestBase {
     @Test
     public void testsGroupDeletion() {
         app.getNavigationHelper().gotoGroupPage();
+        int before = app.getGroupHelper().getGroupCount();
 
         if (! app.getGroupHelper().isThereAGroup()) {
             app.getGroupHelper().createGroup(new GroupData("test1", null, null));
@@ -15,6 +17,8 @@ public class GroupDeletionTests extends TestBase {
         app.getGroupHelper().selectGroup();
         app.getGroupHelper().deleteSelectedGroups();
         app.getGroupHelper().returnToGroupPage();
+        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after, before - 1);
     }
 
 }
