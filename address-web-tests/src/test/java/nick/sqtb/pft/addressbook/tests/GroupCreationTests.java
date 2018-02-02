2e61737659e1bcb8bbe90d73f3e050234b917a84
Zibokk <nick.cornovan@gmail.com>
2018-02-01 17:58:06 -0800

    Лекция 4.6. Сравнение коллекций

--- address-web-tests/src/test/java/nick/sqtb/pft/addressbook/tests/GroupCreationTests.java
+++ address-web-tests/src/test/java/nick/sqtb/pft/addressbook/tests/GroupCreationTests.java
@@ -4,20 +4,27 @@ import nick.sqtb.pft.addressbook.model.GroupData;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+import java.util.List;
+
 
 public class GroupCreationTests extends TestBase {
 
     @Test
     public void testGroupCreation() {
         app.getNavigationHelper().gotoGroupPage();
-        int before = app.getGroupHelper().getGroupCount();
+
+        List<GroupData> before = app.getGroupHelper().getGroupList();
+//        int before = app.getGroupHelper().getGroupCount();
+
         app.getGroupHelper().createGroup(new GroupData("test1", null, null));
 //        app.getGroupHelper().initGroupCreation();
 //        app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
 //        app.getGroupHelper().submitGroupCreation();
 //        app.getGroupHelper().returnToGroupPage();
-        int after = app.getGroupHelper().getGroupCount();
-        Assert.assertEquals(after, before + 1);
+
+        List<GroupData> after = app.getGroupHelper().getGroupList();
+//        int after = app.getGroupHelper().getGroupCount();
+        Assert.assertEquals(after.size(), before.size() + 1);
     }
 
 
