33c939037d23b35998451109aaea14f8b1452ae2
Zibokk <nick.cornovan@gmail.com>
2018-03-15 19:47:29 -0700

    Integer_class

---
+++ java-automation-qa/src/main/java/nick/sqtb/pft/framework/JavaForTesterBook/chap004myfirsttest/IntegerExamplesTest.java
@@ -0,0 +1,31 @@
+package nick.sqtb.pft.framework.JavaForTesterBook.chap004myfirsttest;
+import org.testng.AssertJUnit;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import static org.testng.AssertJUnit.*;
+
+
+public class IntegerExamplesTest {
+
+    @Test
+    public void integerExploration() {
+//        Instantiate an Integer Class
+//        Integer four = new Integer(4);
+
+//        Example with autoboxing for new java
+        Integer four = 4;
+        assertEquals("intValue returns int 4", 4, four.intValue());
+//        Assert.assertEquals(four.intValue(), 4);
+
+//        Example with String
+        Integer five = new Integer("5");
+        assertEquals("intValue returns int 5", 5, five.intValue());
+    }
+
+    @Test
+    public void integerHexString() {
+        Integer eleven = 11;
+        assertEquals("11 becomes b", "b", eleven.toHexString(eleven));
+    }
+
+}
